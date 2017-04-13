1463dd8337842797278c6988e7fd11f0e53b7908
Erik Martin <erik@eriks-imac.local>
2017-04-12 23:57:55 -0700

    Deleted unwanted files that were not being used.

--- QuoteDubDub/BRFlabbyManager.swift
+++
@@ -1,7 +0,0 @@
-//
-//  BRFlabbyManager.swift
-//  QuoteDubDub
-//
-//  Created by Erik Martin on 4/11/17.
-//  Copyright © 2017 Mitchell Sweet. All rights reserved.
-//
--- QuoteDubDub/Test.m
+++
@@ -1,9 +0,0 @@
-//
-//  Test.m
-//  QuoteDubDub
-//
-//  Created by Erik Martin on 4/11/17.
-//  Copyright © 2017 Mitchell Sweet. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
